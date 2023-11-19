55273cd2b4187361be859bfcc6abc15139a1150a
ren130302 <ren130302@DESKTOP-FPKN44H>
2023-11-19 23:58:32 +0900

    Add convert class

---
+++ src/main/java/bencode/json/BIntegerSerializer.java
@@ -0,0 +1,24 @@
+package bencode.json;
+
+import java.io.IOException;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+
+import bencode.BInteger;
+
+public class BIntegerSerializer extends StdSerializer<BInteger> {
+
+	private static final long serialVersionUID = 216720264221714444L;
+
+	public BIntegerSerializer() {
+		super(BInteger.class, true);
+	}
+
+	@Override
+	public void serialize(BInteger value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
+		gen.writeNumber(value.getValue());
+	}
+
+}
